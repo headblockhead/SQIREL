bae18b2c1fe71752444dcc8a8a56f05fc2dfe577
Edward Hesketh <headblockhead@gmail.com>
2023-12-24 23:25:45 +0000

    fix: fix mascot logo overlap, logo.svg is now mascot.svg

--- src/sqirel_quantum.c
+++ src/sqirel_quantum.c
@@ -5,27 +5,25 @@
 #include "sqirel_quantum.h"
 #include "sqirel_types.h"
 
-// key_down marks the keycode as active in the active_keycodes array.
 void key_down(struct key *key, uint_fast8_t keycode, uint_fast8_t layer) {
   (void)key;
   (void)layer;
   active_keycodes[keycode] = true; // Mark the keycode as active.
 }
-// key_up marks the keycode as inactive in the active_keycodes array.
+
 void key_up(struct key *key, uint_fast8_t keycode, uint_fast8_t layer) {
   (void)key;
   (void)layer;
   active_keycodes[keycode] = false; // Mark the keycode as inactive.
 }
 
-// mod_down adds the modifier code to the modifiers variable.
 void mod_down(struct key *key, uint_fast8_t modifier_code, uint_fast8_t layer) {
   (void)key;
   (void)layer;
   modifiers |=
       modifier_code; // OR the modifier code into the modifiers variable.
 }
-// mod_up removes the modifier code from the modifiers variable.
+
 void mod_up(struct key *key, uint_fast8_t modifier_code, uint_fast8_t layer) {
   (void)key;
 
@@ -34,9 +32,6 @@ void mod_up(struct key *key, uint_fast8_t modifier_code, uint_fast8_t layer) {
                                // modifiers variable.
 }
 
-// pass_through_rising passes down the rising function of the key to the first
-// active layer below it (but no layers below the default layer). Equivalent to
-// KC_TRNS when paired with pass_through_falling.
 void pass_through_rising(struct key *key, uint_fast8_t arg,
                          uint_fast8_t layer) {
   (void)arg;
@@ -51,9 +46,6 @@ void pass_through_rising(struct key *key, uint_fast8_t arg,
   }
 }
 
-// pass_through_falling passes down the falling function of the key to the first
-// active layer below it (but no layers below the default layer). Equivalent to
-// KC_TRNS when paired with pass_through_rising.
 void pass_through_falling(struct key *key, uint_fast8_t arg,
                           uint_fast8_t layer) {
   (void)arg;
@@ -68,8 +60,6 @@ void pass_through_falling(struct key *key, uint_fast8_t arg,
   }
 }
 
-// momentary_rising sets the target layer to active when run. Equivalent to
-// MO(layer) when paired with momentary_falling.
 void momentary_rising(struct key *key, uint_fast8_t target_layer,
                       uint_fast8_t layer) {
   (void)key;
@@ -77,8 +67,6 @@ void momentary_rising(struct key *key, uint_fast8_t target_layer,
   layers[target_layer] = true;
 }
 
-// momentary_falling sets the target layer to inactive when run. Equivalent to
-// MO(layer) when paired with momentary_rising.
 void momentary_falling(struct key *key, uint_fast8_t target_layer,
                        uint_fast8_t layer) {
   (void)key;
@@ -86,8 +74,6 @@ void momentary_falling(struct key *key, uint_fast8_t target_layer,
   layers[target_layer] = false;
 }
 
-// toggle toggles the target layer when the key is pressed. Can be
-// assigned to rising or falling functions. Equivalent to TG(layer).
 void toggle(struct key *key, uint_fast8_t target_layer, uint_fast8_t layer) {
   (void)key;
   if (layer == target_layer) {
@@ -97,9 +83,6 @@ void toggle(struct key *key, uint_fast8_t target_layer, uint_fast8_t layer) {
   }
 }
 
-// turn_on turns on the target layer, while disabling all other layers (apart
-// from the default layer). Can be assigned to rising or falling functions.
-// Equivalent to TO(layer).
 void turn_on(struct key *key, uint_fast8_t target_layer, uint_fast8_t layer) {
   (void)key;
   (void)layer;
@@ -110,8 +93,6 @@ void turn_on(struct key *key, uint_fast8_t target_layer, uint_fast8_t layer) {
   layers[target_layer] = true;
 }
 
-// default_set changes the default layer to the target layer. Can be assigned to
-// rising or falling functions. Equivalent to DF(layer).
 void default_set(struct key *key, uint_fast8_t target_layer,
                  uint_fast8_t layer) {
   (void)key;
