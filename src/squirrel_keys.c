b529a1d173e23a55740302a7246167fd68dec23b
Edward Hesketh <headblockhead@gmail.com>
2024-07-28 14:58:50 +0100

    fix: missing active_media_code

--- src/squirrel_keys.c
+++ src/squirrel_keys.c
@@ -4,9 +4,8 @@
 
 // active_keycodes is a list of all keycodes that are currently active.
 bool active_keycodes[256] = {false};
-// media_commands is a list of all media commands that are currently active.
-// Limited to the first 256 media commands, as later commands are extremely
-// uncommon, see: https://www.freebsddiary.org/APC/usb_hid_usages.php - Consumer
-bool media_commands[256] = {false};
+// the current active media code, or 0 if none.
+// see: https://www.freebsddiary.org/APC/usb_hid_usages.php - Consumer
+uint16_t active_media_code = 0;
 // modifiers is a bitfield of all active modifiers.
 uint8_t modifiers = 0;
