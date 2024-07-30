067bf45854ec59bce16c4170d124c468c42f65d5
Edward Hesketh <headblockhead@gmail.com>
2024-07-30 16:02:13 +0100

    feat: move to other system for making key structs

--- include/squirrel_constructors.h
+++ include/squirrel_constructors.h
@@ -1,11 +1,15 @@
 #ifndef SQUIRREL_CONSTRUCTORS_H
 #define SQUIRREL_CONSTRUCTORS_H
 
-#include "squirrel_quantum.h"
 #include "squirrel_types.h"
 
-struct key make_key(uint8_t keycode);
-struct key make_modifier(uint8_t modifier_code);
-struct key make_media(uint16_t media_code);
+// Create a key struct with empty values
+struct key make_key(void);
 
+// Add a standard keycode function to the key struct for the given layer
+void key_add_KC(struct key *key, uint8_t layer, uint8_t keycode);
+// Add a modifier function to the key struct for the given layer
+void key_add_mod(struct key *key, uint8_t layer, uint8_t modifier_code);
+// Add a media function to the key struct for the given layer
+void key_add_media(struct key *key, uint8_t layer, uint16_t media_code);
 #endif
--- src/squirrel_constructors.c
+++ src/squirrel_constructors.c
@@ -2,30 +2,27 @@
 #include "squirrel_quantum.h"
 #include "squirrel_types.h"
 
-// Make a default key struct with the given keycode
-struct key make_key(uint8_t keycode) {
-  return (struct key){
-      .rising = {key_down},
-      .risingargs = {keycode},
-      .falling = {key_up},
-      .fallingargs = {keycode},
-  };
+struct key make_key(void) {
+  return (struct key){};
 }
 
-struct key make_modifier(uint8_t modifier_code) {
-  return (struct key){
-      .rising = {mod_down},
-      .risingargs = {modifier_code},
-      .falling = {mod_up},
-      .fallingargs = {modifier_code},
-  };
+void key_add_KC(struct key *key, uint8_t layer, uint8_t keycode) {
+  key->rising[layer] = key_down;
+  key->risingargs[layer] = keycode;
+  key->falling[layer] = key_up;
+  key->fallingargs[layer] = keycode;
 }
 
-struct key make_media(uint16_t media_code) {
-  return (struct key){
-      .rising = {media_down},
-      .risingargs = {media_code},
-      .falling = {media_up},
-      .fallingargs = {media_code},
-  };
+void key_add_mod(struct key *key, uint8_t layer, uint8_t modifier_code) {
+  key->rising[layer] = mod_down;
+  key->risingargs[layer] = modifier_code;
+  key->falling[layer] = mod_up;
+  key->fallingargs[layer] = modifier_code;
+}
+
+void key_add_media(struct key *key, uint8_t layer, uint16_t media_code) {
+  key->rising[layer] = media_down;
+  key->risingargs[layer] = media_code;
+  key->falling[layer] = media_up;
+  key->fallingargs[layer] = media_code;
 }
